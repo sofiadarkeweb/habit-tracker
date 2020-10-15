9b0cbe119a81a7932b21cf6ea401fd174a91a630
adwebb25 <71591996+adwebb25@users.noreply.github.com>
2020-10-15 14:16:14 +0200

    add js

---
+++ app.js
@@ -0,0 +1,14 @@
+//variables//
+
+const nameInput =document.querySelector(".name-input")
+const descriptionInput =document.querySelector(".description-input")
+const addbBtn =document.querySelector(".submit-button")
+
+//event listeners//
+
+addbBtn.addEventListener('click', containerCreator)
+
+//function//
+function containerCreator(){
+    console.log("testing")
+}
