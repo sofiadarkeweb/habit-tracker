354861ac06fba64be2fdf0ec43d4cd62675f661d
Sofia Darke <sofia.darke@hyperisland.se>
2020-10-22 15:42:53 +0200

    js list working

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const nameInput = document.querySelector(".name-input");
 const descriptionInput = document.querySelector(".description-input");
 const addbBtn = document.querySelector(".submit-button");
 const plantContainer = document.querySelector(".plant-container");
+const plantList = document.querySelector(".plant-list");
 const plantPhoto = document.getElementById("plant-photo").files;
 
 //todays date
@@ -12,10 +13,10 @@ const month = today.getMonth() + 1;
 const year = today.getFullYear();
 
 if (day < 10) {
-  day = '0' + day
+	day = "0" + day;
 }
 if (month < 10) {
-  month = '0' + month
+	month = "0" + month;
 }
 
 const date = document.getElementById("date");
@@ -23,95 +24,110 @@ const date = document.getElementById("date");
 date.innerHTML = year + "-" + month + "-" + day;
 
 //event listeners//
-addbBtn.addEventListener("click", containerCreator)
+addbBtn.addEventListener("click", containerCreator);
 
 //function to create the div with plant information
-function containerCreator(event){
-    event.preventDefault();
-    
-    //Display plant photo
-    
-    //variables to date
-    const date = new Date();
-    const dayOfTheMonth = date.getDate();
+function containerCreator(event) {
+	event.preventDefault();
 
-    //variables to rewater
-    const dueWaterDate = dayOfTheMonth + 5;
-    const daysToRewater = dueWaterDate - dayOfTheMonth;
-    localStorage.setItem("dueWaterDate", dueWaterDate);
-    
-    //variables to fertilize
-    const dueFertilizeDate = dayOfTheMonth + 12;
-    const daysToFertilize = dueFertilizeDate - dayOfTheMonth;
-    localStorage.setItem("dueFertilizeDate", dueFertilizeDate);
-    
-    //variables to repot
-    const dueRepotDate = dayOfTheMonth + 45;
-    const daysToRepot = dueRepotDate - dayOfTheMonth;
-    localStorage.setItem("dueRepotDate", dueRepotDate);
+	// create plant Div
+	const plantDiv = document.createElement("div");
+	/*plantDiv.classList.add("plant-list");*/
 
-//Creates the header/name for the container
-    const newName = document.createElement("h2");    
-    plantContainer.appendChild(newName);
-    newName.innerText = nameInput.value
-    newName.classList.add("plant-name");
+	//Display plant photo
 
-// Clears name input
-    nameInput.value = ""
+	//variables to date
+	const date = new Date();
+	const dayOfTheMonth = date.getDate();
 
-//Creates the description for the plant
-    const newDescription = document.createElement("p");    
-    plantContainer.appendChild(newDescription);
-    newDescription.innerText = descriptionInput.value
-    newDescription.classList.add("info-text");
+	//variables to rewater
+	const dueWaterDate = dayOfTheMonth + 5;
+	const daysToRewater = dueWaterDate - dayOfTheMonth;
+	localStorage.setItem("dueWaterDate", dueWaterDate);
 
-// Clears name input
-    descriptionInput.value = ""
+	//variables to fertilize
+	const dueFertilizeDate = dayOfTheMonth + 12;
+	const daysToFertilize = dueFertilizeDate - dayOfTheMonth;
+	localStorage.setItem("dueFertilizeDate", dueFertilizeDate);
 
-//Creates todo list
-//todo - water
-//Create check button - water
-    const checkWaterBtn = document.createElement("button");
-    plantContainer.appendChild(checkWaterBtn);
-    checkWaterBtn.classList.add("water-btn");
-     if (daysToRewater === 1) {
-        checkWaterBtn.innerText = "Water in " + daysToRewater + " day";
-     } else {
-        checkWaterBtn.innerText = "Water in " + daysToRewater + " days";
-     }
-    checkWaterBtn.addEventListener("click", function(){
-        checkWaterBtn.innerText = "Water in 5 days"
-    }) 
-    checkWaterBtn.classList.add("water-btn");
+	//variables to repot
+	const dueRepotDate = dayOfTheMonth + 45;
+	const daysToRepot = dueRepotDate - dayOfTheMonth;
+	localStorage.setItem("dueRepotDate", dueRepotDate);
 
-//todo - fertilize
-//Create check button - fertilize
-    const fertilizeBtn = document.createElement("button");
-    fertilizeBtn.classList.add("fertilize-btn");
-    plantContainer.appendChild(fertilizeBtn);
-    if (daysToFertilize === 1) {
-        fertilizeBtn.innerText = "Fertilize me in " + daysToFertilize + " day"; 
-    } else {
-        fertilizeBtn.innerText = "Fertilize me in " + daysToFertilize + " days";
-    }
-    fertilizeBtn.addEventListener("click", function(){
-        fertilizeBtn.innerText = "Fertilize me in 12 days"
-    });
+	//Creates the header/name for the container
+	const newName = document.createElement("h2");
+	plantDiv.appendChild(newName);
+	newName.innerText = nameInput.value;
+	newName.classList.add("plant-name");
 
-//todo - repot
-//Create check button - repot
-    const checkRepotBtn = document.createElement("button");
-    checkRepotBtn.classList.add("repot-btn");
-    plantContainer.appendChild(checkRepotBtn);
-    if (daysToRepot === 1) {
-        checkRepotBtn.innerHTML = "Repot me in " + daysToRepot + " day"
-    } else {
-        checkRepotBtn.innerHTML = "Repot me in " + daysToRepot + " days"
-    }
-    checkRepotBtn.addEventListener("click", function(){
-        checkRepotBtn.innerText = "Repot me in 45 days"
-    })
-    
-// Append to plant list
-    document.appendChild(plantContainer);
+	// Clears name input
+	nameInput.value = "";
+
+	//Creates the description for the plant
+	const newDescription = document.createElement("p");
+	plantDiv.appendChild(newDescription);
+	newDescription.innerText = descriptionInput.value;
+	newDescription.classList.add("info-text");
+
+	// Clears name input
+	descriptionInput.value = "";
+
+	//todo - water
+	//Create check button - water
+	const checkWaterBtn = document.createElement("button");
+	plantDiv.appendChild(checkWaterBtn);
+	checkWaterBtn.classList.add("water-btn");
+	if (daysToRewater === 1) {
+		checkWaterBtn.innerText = "Water in " + daysToRewater + " day";
+	} else {
+		checkWaterBtn.innerText = "Water in " + daysToRewater + " days";
+	}
+	checkWaterBtn.addEventListener("click", function () {
+		checkWaterBtn.innerText = "Water in 5 days";
+	});
+	checkWaterBtn.classList.add("water-btn");
+
+	//todo - fertilize
+	//Create check button - fertilize
+	const fertilizeBtn = document.createElement("button");
+	fertilizeBtn.classList.add("fertilize-btn");
+	plantDiv.appendChild(fertilizeBtn);
+	if (daysToFertilize === 1) {
+		fertilizeBtn.innerText = "Fertilize me in " + daysToFertilize + " day";
+	} else {
+		fertilizeBtn.innerText = "Fertilize me in " + daysToFertilize + " days";
+	}
+	fertilizeBtn.addEventListener("click", function () {
+		fertilizeBtn.innerText = "Fertilize me in 12 days";
+	});
+
+	//todo - repot
+	//Create check button - repot
+	const checkRepotBtn = document.createElement("button");
+	checkRepotBtn.classList.add("repot-btn");
+	plantDiv.appendChild(checkRepotBtn);
+	if (daysToRepot === 1) {
+		checkRepotBtn.innerHTML = "Repot me in " + daysToRepot + " day";
+	} else {
+		checkRepotBtn.innerHTML = "Repot me in " + daysToRepot + " days";
+	}
+	checkRepotBtn.addEventListener("click", function () {
+		checkRepotBtn.innerText = "Repot me in 45 days";
+	});
+
+	const binBtn = document.createElement("button");
+	binBtn.classList.add("bin-btn");
+	binBtn.innerHTML = `<i class="fas fa"></i>`;
+	plantDiv.appendChild(binBtn);
+
+	// Append to plant list
+	plantList.appendChild(plantDiv);
 }
+
+//bin button
+
+binBtn.addEventListener("click", function () {
+	const divToDelete = document.getElementByClass("plant-list");
+	divToDelete.remove();
+});
